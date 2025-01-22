bdc739b756ef5fc894ee4e4e98d08706ea469548
Jul Ceballos <jceballos@instrumental.net>
2025-01-22 09:44:17 -0600

    Added hubspot to request.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express()
 const port = 3000
 
 const bodyParser = require('body-parser')
+const hubspot = require('@hubspot/api-client')
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.post('/', (req, res) => {
   console.log(req.body, '<< params received')
+
   res.send({
     deal: 1,
     partner: 1,
